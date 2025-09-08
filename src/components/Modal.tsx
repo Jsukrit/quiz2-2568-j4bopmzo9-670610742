6f1c5ab0f10a75023a1e1644bfa896c86aa17f89
Jsukrit <sukritdn1@gmail.com>
2025-09-08 11:43:39 +0700

    update

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,40 +1,47 @@
-import { useState } from "react";
-import {
-  Modal,
-  TextInput,
-  NumberInput,
-  Select,
-  Button,
-  Stack,
-} from "@mantine/core";
+// import { useState } from "react";
+// // import {
+// //   Modal,
+// //   TextInput,
+// //   NumberInput,
+// //   Select,
+// //   Button,
+// //   Stack,
+// // } from "@mantine/core";
 
-type AddFoodModalProps = {
-  opened: boolean;
-  onClose: () => void;
-  onAdd: (
-    name: string,
-    price: number | string,
-    quantity: number | string,
-    category: string
-  ) => void;
-};
+// // type AddFoodModalProps = {
+// //   opened: boolean;
+// //   onClose: () => void;
+// //   onAdd: (
+// //     name: string,
+// //     price: number | string,
+// //     quantity: number | string,
+// //     category: string
+// //   ) => void;
+// // };
 
-export default function AddFoodModal({}: AddFoodModalProps) {
-  const [name, setName] = useState<string>("");
-  const [price, setPrice] = useState<number | string>(0);
-  const [quantity, setQuantity] = useState<number | string>(0);
-  const [category, setCategory] = useState<string | null>(null);
+// // export default function AddFoodModal({ onAdd }: AddFoodModalProps) {
+// //   const [name, setName] = useState<string>("");
+// //   const [price, setPrice] = useState<number | string>(0);
+// //   const [quantity, setQuantity] = useState<number | string>(0);
+// //   const [category, setCategory] = useState<string | null>(null);
 
-  const handleSubmit = () => {};
+// //   const handleSubmit = () => {
+// //     setName("Name of manu");
+// //     setPrice(0);
+// //     setQuantity(0);
+// //     setCategory("Select a catagory");
+    
+// //   };
+
 
   
 
-  // หากต้องการแปง type string เป็น type number สามารถดูตัวอย่างนี้ได้
-  let val_number: number = Number("500.0");
-  console.log(val_number + 100); // 600.0
+//   // หากต้องการแปง type string เป็น type number สามารถดูตัวอย่างนี้ได้
+//   let val_number: number = Number("500.0");
+//   console.log(val_number + 100); // 600.0
 
-  return {
-    /* Type additional text here. */
+//   return {
+//     /* Type additional text here. */
     
-  };
-}
+//   };
+// }
